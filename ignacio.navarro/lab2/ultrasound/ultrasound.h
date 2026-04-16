5c7b23708ccbc1811a6c6c404060996fea37de4b
nachinchona <ignacio.navarro@est.fi.uncoma.edu.ar>
2026-04-15 22:08:49 -0300

    ultrasonido

--- ignacio.navarro/lab2/ultrasound/ultrasound.h
+++ ignacio.navarro/lab2/ultrasound/ultrasound.h
@@ -1,9 +1,16 @@
-/*
+ /*
+
  * ultrasound: controla el sensor HC-SR04 para medir distancias en cm
+
  */
 
+
 /* ultrasound_get_distance()
- * 	devuelve la distancia en cm del objeto delante del sensor
- * 	devuelve -1 si no existe ningun objeto
+
+ *  devuelve la distancia en cm del objeto delante del sensor
+
+ *  devuelve -1 si no existe ningun objeto
+
  */
-int ultrasound_get_distance(void);
+
+int ultrasound_get_distance(void); 
